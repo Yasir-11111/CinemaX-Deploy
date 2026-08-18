8cd09f3ba79033034752e3b98894297da25de8dc
Mohammad Yasir Hameed <mohammadyasirhameed01@gmail.com>
2026-08-18 06:23:08 +0530

    Fix null favorite movies and remove env files

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -35,89 +35,88 @@ export const dummyTrailers = [
 
 const dummyCastsData = [
   {
-    name: "Milla Jovovich",
+    name: "Aneet Padda",
     profile_path:
-      "https://image.tmdb.org/t/p/original/usWnHCzbADijULREZYSJ0qfM00y.jpg",
+      "https://in.images.search.yahoo.com/images/view;_ylt=AwrKBDaocntqAVwe4Mq9HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkA2YwM2IwOWMxMWMzY2JkMzQwOTUyN2M2N2E2YWNkNmNjBGdwb3MDMzYEaXQDYmluZw--?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Daneet%2Bpadda%26type%3DE211IN714G0%26fr%3Dmcafee%26fr2%3Dpiv-web%26tab%3Dorganic%26ri%3D36&w=1920&h=1080&imgurl=www.masala.com%2Fcloud%2F2025%2F08%2F01%2Faneet-padda-saiyaara.jpg&rurl=https%3A%2F%2Fwww.masala.com%2Fbollywood-news%2Fsaiyaara-aneet-padda-gets-emotional&size=28KB&p=aneet+padda&oid=f03b09c11c3cbd3409527c67a6acd6cc&fr2=piv-web&fr=mcafee&tt=Saiyaara+star+Aneet+Padda+gets+emotional+on+getting+tribute&b=0&ni=21&no=36&ts=&tab=organic&sigr=Uh1GxqHkOQdw&sigb=8WmSS3_9hEYk&sigi=dY8KLVTvcAZI&sigt=zjQh6JHL9c7Q&.crumb=imUuWYRqVlN&fr=mcafee&fr2=piv-web&type=E211IN714G0",
   },
   {
-    name: "Dave Bautista",
-    profile_path:
-      "https://image.tmdb.org/t/p/original/snk6JiXOOoRjPtHU5VMoy6qbd32.jpg",
+    name: "Ahaan Panday",
+    profile_path: "/bollywood/ahaan-panday.jpg",
   },
   {
-    name: "Arly Jover",
+    name: "Ajay Devgn",
     profile_path:
-      "https://image.tmdb.org/t/p/original/zmznPrQ9GSZwcOIUT0c3GyETwrP.jpg",
+      "https://in.images.search.yahoo.com/images/view;_ylt=AwrKAil9c3tq_lIaJUG9HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzE4MzZjMjlkMjVjYzgzYzViY2FhOGQyZWEzMjM0N2IxBGdwb3MDMQRpdANiaW5n?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dajay%2Bdevgan%26type%3DE211IN714G0%26fr%3Dmcafee%26fr2%3Dpiv-web%26tab%3Dorganic%26ri%3D1&w=850&h=850&imgurl=www.whoa.in%2F2023-Whoa%2Fajay-devgan-photos-ajay-devgan-images-ajay-devgan-wallpapers-bollywood-actor-ajay-devgan-bollywood.png&rurl=https%3A%2F%2Fwww.whoa.in%2Fgallery%2Fajay-devgan-photos-ajay-devgan-images-ajay-devgan-wallpapers-bollywood-actor-ajay-devgan-bollywood&size=1357KB&p=ajay+devgan&oid=1836c29d25cc83c5bcaa8d2ea32347b1&fr2=piv-web&fr=mcafee&tt=Ajay+Devgan+Photos+Ajay+Devgan+Images+Ajay+Devgan+Wallpapers&b=0&ni=21&no=1&ts=&tab=organic&sigr=vPL26nY0CZK5&sigb=cbswX3OnV7zG&sigi=c8je_gUr3AlD&sigt=KDmo_BCW6XHc&.crumb=imUuWYRqVlN&fr=mcafee&fr2=piv-web&type=E211IN714G0",
   },
   {
-    name: "Amara Okereke",
+    name: "Salman Khan",
     profile_path:
-      "https://image.tmdb.org/t/p/original/nTSPtzWu6deZTJtWXHUpACVznY4.jpg",
+      "https://in.images.search.yahoo.com/images/view;_ylt=AwrKAimVc3tq7x4eQhW9HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzdiNDE5MzM2NzFiNjYwYjFiOGY3MDMwOGE1MDNlNWJmBGdwb3MDMTAEaXQDYmluZw--?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dsalman%2Bkhan%26ei%3DUTF-8%26type%3DE211IN714G0%26fr%3Dmcafee%26fr2%3Dsa-gp-search%26tab%3Dorganic%26ri%3D10&w=1600&h=1000&imgurl=4kpictures.co.in%2Fwp-content%2Fuploads%2F2024%2F03%2FSalman-Khan.jpg&rurl=https%3A%2F%2F4kpictures.co.in%2Fsalman-khan-the-bollywood-superstar%2F&size=259KB&p=salman+khan&oid=7b41933671b660b1b8f70308a503e5bf&fr2=sa-gp-search&fr=mcafee&tt=Salman+Khan+4K+Wallpapers+%E2%80%93+Bollywood+Superstar+HD&b=0&ni=200&no=10&ts=&tab=organic&sigr=lPsxiKFC6tW_&sigb=MOKMUte8ceIs&sigi=fmoLTPP0o0Nd&sigt=aTX6cLE72GiO&.crumb=imUuWYRqVlN&fr=mcafee&fr2=sa-gp-search&type=E211IN714G0",
   },
   {
-    name: "Fraser James",
+    name: "Shah Rukh Khan",
     profile_path:
-      "https://image.tmdb.org/t/p/original/mGAPQG2OKTgdKFkp9YpvCSqcbgY.jpg",
+      "https://tse3.mm.bing.net/th/id/OIP.Ka7dZOVSaKGqDY1UDeIvGgHaE8?r=0&pid=Api&P=0&h=180",
   },
   {
-    name: "Deirdre Mullins",
+    name: "Aamir Khan",
     profile_path:
-      "https://image.tmdb.org/t/p/original/lJm89neuiVlYISEqNpGZA5kTAnP.jpg",
+      "https://in.images.search.yahoo.com/images/view;_ylt=AwrKAikTdHtqrHcbH6i9HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkA2JhOTAxNjkyNDdhOTNhZjNkNjQ5NDdlOTNhODRlNDQ4BGdwb3MDMgRpdANiaW5n?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Damir%2Bkhan%26ei%3DUTF-8%26type%3DE211IN714G0%26fr%3Dmcafee%26fr2%3Dsa-gp-search%26tab%3Dorganic%26ri%3D2&w=1200&h=900&imgurl=cdn.siasat.com%2Fwp-content%2Fuploads%2F2022%2F07%2FAmir-khan.jpg&rurl=https%3A%2F%2Fwww.siasat.com%2Faamir-khan-on-making-mahabharat-i-am-afraid-2385830%2F&size=27KB&p=amir+khan&oid=ba90169247a93af3d64947e93a84e448&fr2=sa-gp-search&fr=mcafee&tt=Aamir+Khan+on+making+Mahabharat%2C+%26quot%3BI+am+afraid%26quot%3B&b=0&ni=200&no=2&ts=&tab=organic&sigr=lBdItJ.BRWvh&sigb=w6g.2_NknFH5&sigi=zAS6YbRfjL7k&sigt=AJNFuowOC9B7&.crumb=imUuWYRqVlN&fr=mcafee&fr2=sa-gp-search&type=E211IN714G0",
   },
   {
-    name: "Sebastian Stankiewicz",
+    name: "Deepika Padukone",
     profile_path:
-      "https://image.tmdb.org/t/p/original/hLN0Ca09KwQOFLZLPIEzgTIbqqg.jpg",
+      "https://in.images.search.yahoo.com/images/view;_ylt=AwrKAilUdHtqROQd2DW9HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzEyNWJkMzBmZjBjOTQzYTRiMTFmZDg1M2UzNGEwNTE5BGdwb3MDMTIEaXQDYmluZw--?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Ddeepika%2Bpadukone%26type%3DE211IN714G0%26fr%3Dmcafee%26fr2%3Dpiv-web%26tab%3Dorganic%26ri%3D12&w=3840&h=2160&imgurl=wallpapercave.com%2Fwp%2Fwp6788307.jpg&rurl=https%3A%2F%2Fwallpapercave.com%2Fdeepika-padukone-close-up-hd-wallpapers&size=707KB&p=deepika+padukone&oid=125bd30ff0c943a4b11fd853e34a0519&fr2=piv-web&fr=mcafee&tt=Deepika+Padukone+Close+Up+HD+Wallpapers+-+Wallpaper+Cave&b=0&ni=21&no=12&ts=&tab=organic&sigr=3YEmEkXRVT1E&sigb=2McnvRsONyT5&sigi=kXsw4ax1zrSA&sigt=ab6Kq2j1IaYP&.crumb=imUuWYRqVlN&fr=mcafee&fr2=piv-web&type=E211IN714G0",
   },
   {
-    name: "Tue Lunding",
+    name: "Ranveer Singh",
     profile_path:
-      "https://image.tmdb.org/t/p/original/qY4W0zfGBYzlCyCC0QDJS1Muoa0.jpg",
+      "https://in.images.search.yahoo.com/images/view;_ylt=AwrKAimLdHtq6nUcova9HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkA2MyZTZjYjg1YTE1NDNkNGE0MmFkNTdkNGE1ODMwODE0BGdwb3MDMQRpdANiaW5n?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dranbir%2Bsingh%26ei%3DUTF-8%26type%3DE211IN714G0%26fr%3Dmcafee%26fr2%3Dsa-gp-search%26tab%3Dorganic%26ri%3D1&w=1200&h=900&imgurl=thedailyguardian.com%2Fwp-content%2Fuploads%2F2025%2F11%2Franveer-singh.png&rurl=https%3A%2F%2Fthedailyguardian.com%2Fglamorama%2Fwho-is-ranveer-singh-age-wife-career-movies-net-worth-controversies-681419%2F&size=762KB&p=ranbir+singh&oid=c2e6cb85a1543d4a42ad57d4a5830814&fr2=sa-gp-search&fr=mcafee&tt=Who+is+Ranveer+Singh%3F+Age%2C+Wife%2C+Career%2C+Movies%2C+Net+Worth+%26+Controversies&b=0&ni=200&no=1&ts=&tab=organic&sigr=hxOAR1MCrQL4&sigb=6wPprOEDq2_S&sigi=GcRpgjuNMFUi&sigt=yg1v7rTa12Wf&.crumb=imUuWYRqVlN&fr=mcafee&fr2=sa-gp-search&type=E211IN714G0",
   },
   {
-    name: "Jacek Dzisiewicz",
+    name: "Ranbir Kapoor",
     profile_path:
-      "https://image.tmdb.org/t/p/original/6Ksb8ANhhoWWGnlM6O1qrySd7e1.jpg",
+      "https://in.images.search.yahoo.com/images/view;_ylt=AwrKAilCdXtq5Y8c6.i9HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzc1NmNiODRjNzg4NjZiNDAwZGVmNjFmMDVjMzQxYWE0BGdwb3MDNjMEaXQDYmluZw--?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dranbir%2Bkapoor%26ei%3DUTF-8%26type%3DE211IN714G0%26fr%3Dmcafee%26fr2%3Dsa-gp-search%26nost%3D1%26tab%3Dorganic%26ri%3D63&w=474&h=248&imgurl=media.assettype.com%2Fdeccanherald%2F2026-04-16%2F2bpoawme%2FRanbir-Kapoor.jpg%3Frect%3D0%252C0%252C647%252C340%26w%3D1200%26ar%3D40%3A21%26auto%3Dformat%252Ccompress%26ogImage%3Dtrue%26mode%3Dcrop&rurl=https%3A%2F%2Fwww.deccanherald.com%2Fentertainment%2Ftime-100-2026-ranbir-kapoor-becomes-the-sole-indian-actor-to-earn-a-spot-in-this-years-list-3969722&size=100KB&p=ranbir+kapoor&oid=756cb84c78866b400def61f05c341aa4&fr2=sa-gp-search&fr=mcafee&tt=TIME+100+2026%3A+Ranbir+Kapoor+only+Indian+actor+on+influential+global+lists&b=61&ni=168&no=63&ts=&tab=organic&sigr=X3QRrgyMwRoG&sigb=uvsd5vfogf6F&sigi=lOYTCXyaer8L&sigt=OVX8A8OFbXZ_&.crumb=imUuWYRqVlN&fr=mcafee&fr2=sa-gp-search&type=E211IN714G0",
   },
   {
-    name: "Ian Hanmore",
+    name: "Alia Bhatt",
     profile_path:
-      "https://image.tmdb.org/t/p/original/yhI4MK5atavKBD9wiJtaO1say1p.jpg",
+      "https://in.images.search.yahoo.com/images/view;_ylt=AwrKAin.dHtqhngbO5C9HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkA2M0ZTg2OGFmZDE5MmEwMjM1YzBjNzUwZmRiY2M0ZDUyBGdwb3MDMTA5BGl0A2Jpbmc-?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dalia%2Bbhatt%26type%3DE211IN714G0%26fr%3Dmcafee%26fr2%3Dpiv-web%26nost%3D1%26tab%3Dorganic%26ri%3D109&w=1920&h=1080&imgurl=assets.gqindia.com%2Fphotos%2F63033b1a0f61f07125bf9683%2F16%3A9%2Fw_2560%252Cc_limit%2FAlia-Bhatt_01.jpeg&rurl=https%3A%2F%2Fwww.gqindia.com%2Fentertainment%2Fcontent%2Fheres-how-much-money-alia-bhatt-was-paid-for-her-first-film-student-of-the-year-and-its-nothing-compared-to-her-current-fees&size=187KB&p=alia+bhatt&oid=c4e868afd192a0235c0c750fdbcc4d52&fr2=piv-web&fr=mcafee&tt=Here%E2%80%99s+how+much+money+Alia+Bhatt+was+paid+for+her+first+film+Student+of+...&b=61&ni=21&no=109&ts=&tab=organic&sigr=fQAVgsWenpvh&sigb=eNUt_LVPa1jy&sigi=a1jxpiMTyZQg&sigt=ffj2XxWE7g3U&.crumb=imUuWYRqVlN&fr=mcafee&fr2=piv-web&type=E211IN714G0",
   },
   {
-    name: "Eveline Hall",
+    name: "Kartik Aaryan",
     profile_path:
-      "https://image.tmdb.org/t/p/original/uPq4xUPiJIMW5rXF9AT0GrRqgJY.jpg",
+      "https://in.images.search.yahoo.com/images/view;_ylt=AwrKAil1dXtqmr4bk2S9HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzgwMGFmNWJlZmMyYzU0YzhkYjEwYTgyNDI5NWUwNmZiBGdwb3MDMQRpdANiaW5n?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dkartik%2Baaryan%26ei%3DUTF-8%26type%3DE211IN714G0%26fr%3Dmcafee%26fr2%3Dsa-gp-search%26tab%3Dorganic%26ri%3D1&w=1920&h=1080&imgurl=i.cdn.newsbytesapp.com%2Fimages%2Fl41620231121135337.jpeg&rurl=https%3A%2F%2Fwww.newsbytesapp.com%2Fnews%2Fentertainment%2Fkartik-aaryan-best-dialogues%2Fstory&size=54KB&p=kartik+aaryan&oid=800af5befc2c54c8db10a824295e06fb&fr2=sa-gp-search&fr=mcafee&tt=Happy+birthday%2C+Kartik+Aaryan%3A+5+best+dialogues+of+the+actor&b=0&ni=168&no=1&ts=&tab=organic&sigr=hBDEUv3bidcO&sigb=6IsHemGNZVCZ&sigi=yXrNnQHwbHFU&sigt=oVJmBS5_4cD.&.crumb=imUuWYRqVlN&fr=mcafee&fr2=sa-gp-search&type=E211IN714G0",
   },
   {
-    name: "Kamila Klamut",
+    name: "Rashmika Mandanna",
     profile_path:
-      "https://image.tmdb.org/t/p/original/usWnHCzbADijULREZYSJ0qfM00y.jpg",
+      "https://in.images.search.yahoo.com/images/view;_ylt=AwrKBDbAdXtqsj8dSH69HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzJmZDc2MGVkY2U3Njg3YzUyZjRlNDU2YjU5ZTQ5Yjc0BGdwb3MDNgRpdANiaW5n?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Drashmika%2Bmandanna%26type%3DE211IN714G0%26fr%3Dmcafee%26fr2%3Dpiv-web%26tab%3Dorganic%26ri%3D6&w=1200&h=630&imgurl=img.indianwitness.com%2Flarge%2Frashmika--3534.webp&rurl=https%3A%2F%2Fwww.indianwitness.com%2Flifestyle%2Fhoroscope%2Frashmika-mandanna-on-choosing-roles-i-want-to-play-dive&size=32KB&p=rashmika+mandanna&oid=2fd760edce7687c52f4e456b59e49b74&fr2=piv-web&fr=mcafee&tt=Rashmika+Mandanna+on+Choosing+Roles%3A+%E2%80%9CI+Want+to+Play+Diverse+Characters+...&b=0&ni=21&no=6&ts=&tab=organic&sigr=plyF2TXn8BTX&sigb=YvzmtCTqlvC8&sigi=FNQp3C4jMvte&sigt=uCv94e.lSlQO&.crumb=imUuWYRqVlN&fr=mcafee&fr2=piv-web&type=E211IN714G0",
   },
   {
-    name: "Caoilinn Springall",
+    name: "Kriti Sanon",
     profile_path:
-      "https://image.tmdb.org/t/p/original/uZNtbPHowlBYo74U1qlTaRlrdiY.jpg",
+      "https://in.images.search.yahoo.com/images/view;_ylt=AwrKAinidXtq4BoaEly9HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzk2NDMyZmY2Zjg1MzI1YzNjNmM3ZGI2Y2UzNTUwNWMxBGdwb3MDNgRpdANiaW5n?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dkriti%2Bsanon%26ei%3DUTF-8%26type%3DE211IN714G0%26fr%3Dmcafee%26fr2%3Dsa-gp-search%26tab%3Dorganic%26ri%3D6&w=1080&h=1349&imgurl=wallpapers.com%2Fimages%2Fhd%2Fkriti-sanon-animal-print-hd-ocoxufhnbm3a6j8t.jpg&rurl=https%3A%2F%2Ftnhelearning.edu.vn%2Fkriti-sanon-hd-wallpaper%2F&size=167KB&p=kriti+sanon&oid=96432ff6f85325c3c6c7db6ce35505c1&fr2=sa-gp-search&fr=mcafee&tt=Top+999%2B+Kriti+Sanon+Hd+Wallpaper+Full+HD%2C+4K+Free+to+Use&b=0&ni=200&no=6&ts=&tab=organic&sigr=s_zSd_U0S7gS&sigb=eJrQHobWLDwj&sigi=ibD1WYuYCTto&sigt=dSRT1DnkGPCe&.crumb=imUuWYRqVlN&fr=mcafee&fr2=sa-gp-search&type=E211IN714G0",
   },
   {
-    name: "Jan Kowalewski",
+    name: "Akshay Kumar",
     profile_path:
-      "https://image.tmdb.org/t/p/original/snk6JiXOOoRjPtHU5VMoy6qbd32.jpg",
+      "https://www.imdb.com/name/nm0474774/mediaviewer/rm2126386432/?ref_=nm_ov_ph",
   },
   {
-    name: "Pawel Wysocki",
+    name: "Varun Dhawan",
     profile_path:
-      "https://image.tmdb.org/t/p/original/zmznPrQ9GSZwcOIUT0c3GyETwrP.jpg",
+      "https://in.images.search.yahoo.com/images/view;_ylt=AwrKAinMdntqFCIdHpq9HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkA2Q0YjAwODdhNDI0YTk0YTE3NjcxNWIyOWUwZGNlNDNmBGdwb3MDNQRpdANiaW5n?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dvarun%2Bdhawan%26type%3DE211IN714G0%26fr%3Dmcafee%26fr2%3Dpiv-web%26tab%3Dorganic%26ri%3D5&w=1000&h=1000&imgurl=www.pinkvilla.com%2Fimages%2F2025-02%2F2108283907_varun-dhawan.jpg&rurl=https%3A%2F%2Fwww.pinkvilla.com%2Fentertainment%2Fexclusives%2Fexclusive-varun-dhawan-and-kriti-sanons-bhediya-now-release-year-end-due-heavy-visual-effects-work-947642&size=119KB&p=varun+dhawan&oid=d4b0087a424a94a176715b29e0dce43f&fr2=piv-web&fr=mcafee&tt=EXCLUSIVE%3A+Varun+Dhawan+and+Kriti+Sanon%26%2339%3Bs+Bhediya+to+now+release+by+...&b=0&ni=21&no=5&ts=&tab=organic&sigr=2m_eeR97gt0o&sigb=SB02IdvNn001&sigi=9nUa4M57hqlK&sigt=cj2usIdzH05J&.crumb=imUuWYRqVlN&fr=mcafee&fr2=piv-web&type=E211IN714G0",
   },
   {
-    name: "Simon Lööf",
+    name: "Tiger Shroff",
     profile_path:
-      "https://image.tmdb.org/t/p/original/cbZrB8crWlLEDjVUoak8Liak6s.jpg",
+      "https://in.images.search.yahoo.com/images/view;_ylt=AwrKAin4dntqVkYcphK9HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzBmNGE4ZDQzNzM2OTc1Zjk0MDdmODkyNmE5NGI4OWYwBGdwb3MDOTYEaXQDYmluZw--?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dtiger%2Bshroff%26ei%3DUTF-8%26type%3DE211IN714G0%26fr%3Dmcafee%26fr2%3Dsa-gp-search%26nost%3D1%26tab%3Dorganic%26ri%3D96&w=612&h=408&imgurl=i.pinimg.com%2Foriginals%2F0e%2Fdd%2Fa1%2F0edda18ec28e7d03f88d8bc4a0d4ec78.jpg&rurl=https%3A%2F%2Fwww.artofit.org%2Fimage-gallery%2F609182287100107073%2Fbollywood-actor-tiger-shroff-poses-during-an-exclusive-interview-with%2F&size=19KB&p=tiger+shroff&oid=0f4a8d43736975f9407f8926a94b89f0&fr2=sa-gp-search&fr=mcafee&tt=Bollywood+actor+tiger+shroff+poses+during+an+exclusive+interview+with+...&b=61&ni=200&no=96&ts=&tab=organic&sigr=ED4lXLLMnCGe&sigb=PWrdrFLZ7wZD&sigi=J3f7kS3Zpdk7&sigt=Akiu_x1yOuVO&.crumb=imUuWYRqVlN&fr=mcafee&fr2=sa-gp-search&type=E211IN714G0",
   },
   {
-    name: "Tomasz Cymerman",
+    name: " Kiara Advani",
     profile_path:
-      "https://image.tmdb.org/t/p/original/nTSPtzWu6deZTJtWXHUpACVznY4.jpg",
+      "https://in.images.search.yahoo.com/images/view;_ylt=AwrKAinDd3tqxBIc6zO9HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzM4MTVlY2MwMWI4YzgxYjc3YmNmODNkMTMxYjBmMjgwBGdwb3MDMTQEaXQDYmluZw--?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3DKiara%2BAdvani%26type%3DE211IN714G0%26fr%3Dmcafee%26fr2%3Dpiv-web%26tab%3Dorganic%26ri%3D14&w=1920&h=1080&imgurl=www.bollywoodhungama.com%2Fwp-content%2Fuploads%2F2022%2F10%2FKiara-Advani-1-10.jpg&rurl=https%3A%2F%2Fwww.bollywoodhungama.com%2Fphotos%2Fceleb-wallpapers%2Fkiara-advani%2Fkiara-advani-1-105%2F&size=651KB&p=Kiara+Advani&oid=3815ecc01b8c81b77bcf83d131b0f280&fr2=piv-web&fr=mcafee&tt=Wallpapers+%7C+-+Bollywood+Hungama&b=0&ni=21&no=14&ts=&tab=organic&sigr=y4k04HVMI4Ak&sigb=ynAlJj2RVgwV&sigi=Rhhh7ln2hqHI&sigt=caLcaC5POVLh&.crumb=imUuWYRqVlN&fr=mcafee&fr2=piv-web&type=E211IN714G0",
   },
 ];
 
